0ad3b78db6c14c60a03c6eb96ea316554d69d9d5
Shubham Pande <shubham.pande.in@gmail.com>
2026-08-19 19:54:32 +0530

    Update DuckDNS IP in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-curl -s "https://www.duckdns.org/update?domains=myfocusai,myfocusapi&token=d19668ba-b5b6-42af-90fe-e2369ab42ee1&ip="
+curl -s "https://www.duckdns.org/update?domains=myfocusai,myfocusapi&token=d19668ba-b5b6-42af-90fe-e2369ab42ee1&ip=100.53.69.47"
 echo "DuckDNS Updated"
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
